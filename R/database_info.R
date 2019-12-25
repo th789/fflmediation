b0e4e5f3419412d80c007f19713e95c559e18e4f
th <than@dhcp-10-250-125-11.harvard.edu>
2019-12-24 22:27:05 -0500

    3 interactions databases

--- R/database_info.R
+++ R/database_info.R
@@ -1,10 +1,37 @@
 
-###miRNA-target gene databases
+###TF-miRNA interactions
+#' @title Transcription Factor (TF)-miRNA Interactions
+#' @description A DataFrame of TF-miRNA interactions
+#' compiled from databases (TransmiR v2.0).
+#' @format A DataFrame (117543 x 3) with the following columns:
+#' \describe{
+#' \item{tf}{TF involved in the interaction}
+#' \item{mirna}{miRNA involved in the interaction (miRNA targeted by TF)}
+#' \item{TRANSMIR}{indicator of whether interaction is in TransmiR v2.0 database}
+#' }
+"tf_mirna_db"
+
+
+###TF-target gene interactions
+#' @title Transcription Factor (TF)-Target Gene Interactions
+#' @description A DataFrame of TF-target gene interactions
+#' compiled from databases (ENCODE and TRRUST v2.0).
+#' @format A DataFrame (1624664 x 4) with the following columns:
+#' \describe{
+#' \item{tf}{TF involved in the interaction}
+#' \item{targetgene}{target gene involved in the interaction (gene targeted by TF)}
+#' \item{TRRUST}{indicator of whether interaction is in TRRUST v2.0 database}
+#' \item{ENCODE}{indicator of whether interaction is in ENCODE database}
+#' }
+"tf_targetgene_db"
+
+
+###miRNA-target gene interactions
 #' @title miRNA-Target Gene Interactions
 #' @description A DataFrame of miRNA-target gene interactions
 #' compiled from databases (TargetScan v7.2, miRTarBase v7.0,
 #' miRDB v6.0, and miRanda v3.0).
-#' @format A DataFrame (1757820 x 6) with the following columns:
+#' @format A DataFrame (2477728 x 6) with the following columns:
 #' \describe{
 #' \item{mirna}{miRNA involved in the interaction}
 #' \item{targetgene}{target gene involved in the interaction (gene targeted by miRNA)}
