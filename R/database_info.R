27484229fa5030085bc7898f37371c57d0ed037e
th <than@dhcp-10-250-125-11.harvard.edu>
2019-12-26 22:19:44 -0500

    3 databases, added name info

--- R/database_info.R
+++ R/database_info.R
@@ -5,8 +5,8 @@
 #' compiled from databases (TransmiR v2.0).
 #' @format A DataFrame (117543 x 3) with the following columns:
 #' \describe{
-#' \item{tf}{TF involved in the interaction}
-#' \item{mirna}{miRNA involved in the interaction (miRNA targeted by TF)}
+#' \item{tf}{TF involved in the interaction [Ensembl ID]}
+#' \item{mirna}{miRNA involved in the interaction (miRNA targeted by TF) [Accession Number]}
 #' \item{TRANSMIR}{indicator of whether interaction is in TransmiR v2.0 database}
 #' }
 "tf_mirna_db"
@@ -18,8 +18,8 @@
 #' compiled from databases (ENCODE and TRRUST v2.0).
 #' @format A DataFrame (1624664 x 4) with the following columns:
 #' \describe{
-#' \item{tf}{TF involved in the interaction}
-#' \item{targetgene}{target gene involved in the interaction (gene targeted by TF)}
+#' \item{tf}{TF involved in the interaction [Ensembl ID]}
+#' \item{targetgene}{target gene involved in the interaction (gene targeted by TF) [Ensembl ID]}
 #' \item{TRRUST}{indicator of whether interaction is in TRRUST v2.0 database}
 #' \item{ENCODE}{indicator of whether interaction is in ENCODE database}
 #' }
@@ -33,8 +33,8 @@
 #' miRDB v6.0, and miRanda v3.0).
 #' @format A DataFrame (2477728 x 6) with the following columns:
 #' \describe{
-#' \item{mirna}{miRNA involved in the interaction}
-#' \item{targetgene}{target gene involved in the interaction (gene targeted by miRNA)}
+#' \item{mirna}{miRNA involved in the interaction [Accession Number]}
+#' \item{targetgene}{target gene involved in the interaction (gene targeted by miRNA) [Ensembl ID]}
 #' \item{TARGETSCAN}{indicator of whether interaction is in TargetScan v7.2 database}
 #' \item{MIRTARBASE}{indicator of whether interaction is in miRTarBase v7.0 database}
 #' \item{MIRDB}{indicator of whether interaction is in miRDB v6.0 database}
