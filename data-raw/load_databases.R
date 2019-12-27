1101ba61b6431daa18e4a647b4e6de6c802a7bb6
th <than@dhcp-10-250-125-11.harvard.edu>
2019-12-26 22:15:27 -0500

    3 interactions databases, fixed

--- data-raw/load_databases.R
+++ data-raw/load_databases.R
@@ -1,15 +1,15 @@
-
-
-#tf-mirna interactions
-load(file = "/Users/than/Dropbox/research/work_draft/databases/tf_mirna_db.rda")
-usethis::use_data(tf_mirna_db, compress = "xz")
-
-#tf-targetgene interactions
-load(file = "/Users/than/Dropbox/research/work_draft/databases/tf_targetgene_db.rda")
-usethis::use_data(tf_targetgene_db, compress = "xz")
-
-#mirna-targetgene interactions
-load(file = "/Users/than/Dropbox/research/work_draft/databases/mirna_targetgene_db.rda")
-usethis::use_data(mirna_targetgene_db, compress = "xz")
-
-
+#
+#
+# #tf-mirna interactions
+# load(file = "/Users/than/Dropbox/research/work_draft/databases/tf_mirna_db.rda")
+# usethis::use_data(tf_mirna_db, compress = "xz")
+#
+# #tf-targetgene interactions
+# load(file = "/Users/than/Dropbox/research/work_draft/databases/tf_targetgene_db.rda")
+# usethis::use_data(tf_targetgene_db, compress = "xz")
+#
+# #mirna-targetgene interactions
+# load(file = "/Users/than/Dropbox/research/work_draft/databases/mirna_targetgene_db.rda")
+# usethis::use_data(mirna_targetgene_db, compress = "xz")
+#
+#
